f9c756ec1911d463bf0ed569b227bbb034a8d354
Kazi Foisal <kazimdfoisal7409@gmail.com>
2025-08-10 18:37:23 +0600

    fetching by UseEffect()

--- src/App.jsx
+++ src/App.jsx
@@ -1,171 +1,27 @@
-// import React, { useState } from "react";
-// import "./App.css"
-// const App = () => {
-//   const [list, setList] = useState([]);
-//   const [items, setitems] = useState("");
 
-//   const addToDO = () => {
-//     list.push(items);
-//     setList([...list]);
-//     console.log(list);
-//   };
 
-//   const removeToDo=(index)=>{
-//      list.splice(index,1);
-//      setList([...list])
-
-//      console.log(index);
-//   }
-
-//   return (
-//     <div className="whole_project">
-//      <div className="initial_part">
-//      <input
-//         onChange={(e) => setitems(e.target.value)}
-//         type="text"
-//         placeholder="Lists"
-//       />
-//       <button onClick={addToDO}>Add To Do</button>
-//      </div>
-
-//      <table>
-//       <tbody>
-//           {list.length !== 0 ? (
-//             list.map((element, index) => {
-//               return (
-//                 <tr key={index}>
-//                   <td>{element}</td>
-//                   <td>
-//                     <button onClick={()=>removeToDo(index)}>Remove</button>
-//                   </td>
-//                 </tr>
-//               );
-//             })
-//           ) : (
-//             <tr><td>Empty</td></tr>
-//           )}
-//       </tbody>
-//      </table>
-//     </div>
-//   );
-// };
-
-// export default App;
-
-// import React, { useState } from "react";
-// import "./App.css"
-
-// const App = () => {
-//   const [list, setList] = useState([]);
-//   const [items, setItems] = useState({
-//     firstName: "",
-//     lastName: "",
-//     email: "",
-//     password: "",
-//   });
-
-//   const create = () => {
-//     list.push(items);
-//     setList([...list]);
-//   };
-
-//   return (
-//     <div className="whole_project">
-//       <div className="form">
-//         <div className="fullName">
-//           <input
-//             onChange={(e) =>
-//               setItems((prev) => ({
-//                 ...prev,
-//                 firstName: e.target.value,
-//               }))
-//             }
-//             type="text"
-//             placeholder="firstName"
-//             className="firstName"
-//           />
-//           <input
-//             onChange={(e) =>
-//               setItems((prev) => ({
-//                 ...prev,
-//                 lastName: e.target.value,
-//               }))
-//             }
-//             type="text"
-//             placeholder="lastName"
-//             className="lastName"
-//           />
-//         </div>
-//         <input
-//           onChange={(e) =>
-//             setItems((prev) => ({
-//               ...prev,
-//               email: e.target.value,
-//             }))
-//           }
-//           type="email"
-//           placeholder="e-mail"
-//         />
-//         <input
-//           onChange={(e) =>
-//             setItems((prev) => ({
-//               ...prev,
-//               password: e.target.value,
-//             }))
-//           }
-//           type="password"
-//           placeholder="password"
-//         />
-//         <div className="btn">
-//           <button onClick={create}>Create Account</button>
-//         </div>
-//       </div>
-
-//       <table>
-//         <tbody>
-//           {list.length !== 0 ? (
-//             list.map((element,index) => {
-//               return(
-//                <tr key={index}>
-//                 <td>{element.firstName}</td>
-//                 <td>{element.lastName}</td>
-//                 <td>{element.email}</td>
-//                 <td>{element.password}</td>
-//               </tr>
-//               )
-//             })
-//           ) : (
-//             <tr><td>Empty</td></tr>
-//           )}
-//         </tbody>
-//       </table>
-//     </div>
-//   );
-// };
-
-// export default App;
-
-//
-
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState } from 'react'
 
 const App = () => {
-  const [count, setCount] = useState(0);
 
-  const clickMe = () => {
-    setCount((prev) => prev + 1);
-  };
+     const[data,setdata]=useState();
 
-  useEffect(() => {
-    console.log("Count changed:", count);
-  }, [count]);
+useEffect(()=>{
+     (async()=>{
+     let response =  await fetch('https://dummyjson.com/carts');
+
+      let result = await response.json();
+     setdata(result);
+
+     })()
+},[])
 
   return (
     <div>
-      <h1>{count}</h1>
-      <button onClick={clickMe}>Click</button>
+      {JSON.stringify(data)}
     </div>
-  );
-};
+  )
+}
 
 export default App;
+
